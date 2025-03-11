f5c79bba6e4d770655c46776662c20eca5b2ca53
Eyobel Mebratie <eyobelmebratie9@gmail.com>
2025-03-11 17:59:37 +0300

    editing fonts

--- lib/description/description.dart
+++ lib/description/description.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_ordering_app/splash/splash.dart';
 
 void main() {
   runApp(MyApp());
@@ -17,13 +18,13 @@ class DescriptionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 33, 89, 243),
+      backgroundColor: const Color(0xFF2C3DD8),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'assets/images/first.png', // Replace with your local image path
+              'assets/images/first.png',
               height: 150,
               width: 150,
             ),
@@ -40,6 +41,16 @@ class DescriptionScreen extends StatelessWidget {
                   style: TextStyle(fontSize: 18),
                 ),
               ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SplashScreen()),
+                );
+              },
+              child: Text('Go to Splash'),
             ),
           ],
         ),
--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -23,7 +23,7 @@ class SplashScreen extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Image.asset(
-              'assets/images/splash.png', // Replace with your local image path
+              'assets/images/splash.png',
               height: 150,
               width: 150,
             ),
