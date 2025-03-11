9d913589c0cad24f691f64efa1dcccc91d0c5814
Eyobel Mebratie <eyobelmebratie9@gmail.com>
2025-03-11 15:58:57 +0300

    adding the first page with bg color and adding images and text

---
+++ lib/description/description.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DescriptionScreen(),
+    );
+  }
+}
+
+class DescriptionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 33, 89, 243),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/first.png', // Replace with your local image path
+              height: 150,
+              width: 150,
+            ),
+            SizedBox(height: 20),
+            Card(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  '''Profile:
+                  Name: Eyobel Mebratie
+                  Group Number: 6
+                  Fav Food: Pizza''',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
