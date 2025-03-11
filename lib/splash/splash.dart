600a13a7a617b05212e7fdca89d1ea0bd63d4455
Eyobel Mebratie <eyobelmebratie9@gmail.com>
2025-03-11 16:35:31 +0300

    added the splash scrren

---
+++ lib/splash/splash.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 33, 89, 243),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/splash.png', // Replace with your local image path
+              height: 150,
+              width: 150,
+            ),
+            SizedBox(height: 20),
+            Card(
+              color: Colors.white,
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Text(
+                  'Enjoy Your Food',
+                  style: TextStyle(fontSize: 38),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Action when button is pressed
+              },
+              child: Text('Get Started'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
